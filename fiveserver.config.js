2561f80cb86c556d70729a6b1f0a026d84e15962
bcrhbrhcdb <apet2804@mpsedu.org>
2024-05-06 11:25:47 -0500

    uovuvgkhvgh

--- fiveserver.config.js
+++ fiveserver.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   watch: ['src', 'public'], // Watch for changes in src and public folders
-  ignore: [/\.s[ac]ss$/i, /\.tsx?$/i] // Ignore .sass, .scss, .ts, and .tsx files
+  ignore: [/\.s[ac]ss$/i, /\.tsx?$/i], // Ignore .sass, .scss, .ts, and .tsx files
+  reload: true // Reload the browser automatically when changes are made
 }
