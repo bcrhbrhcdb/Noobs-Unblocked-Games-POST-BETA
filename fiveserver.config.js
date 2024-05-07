81b0c3c656356961729990a0371845e8bfb12989
bcrhbrhcdb <123518949+bcrhbrhcdb@users.noreply.github.com>
2024-05-07 10:41:46 -0500

    Delete fiveserver.config.js

--- fiveserver.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  watch: ['src', 'public'], // Watch for changes in src and public folders
-  ignore: [/\.s[ac]ss$/i, /\.tsx?$/i], // Ignore .sass, .scss, .ts, and .tsx files
-  reload: true // Reload the browser automatically when changes are made
-}
