af0db5be6dbbfd589ba45002f062d7d5aa9d9ecf
bcrhbrhcdb <apet2804@mpsedu.org>
2024-05-07 11:00:54 -0500

    redid package.json

---
+++ bs-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "files": ["./**/*.{html,htm,css,js}"],
+    "watchOptions": {
+      "ignored": "node_modules"
+    },
+    "server": {
+      "baseDir": "./"
+    },
+    "open": "local" // Add this line to open the browser automatically
+  };
