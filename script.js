471d0aef6cc91b6ebbc47b09879fd8e1aa84ff2c
bcrhbrhcdb <apet2804@mpsedu.org>
2024-05-08 12:09:24 -0500

    j

--- script.js
+++ script.js
@@ -55,6 +55,16 @@ document.addEventListener('DOMContentLoaded', () => {
     // Initial check to see which games are in view
     checkPosition();
 });
+//annoucement
+document.addEventListener('DOMContentLoaded', function() {
+    var announcementBanner = document.getElementById('announcement-banner');
+    var closeButton = announcementBanner.querySelector('.close-button');
+  
+    // Close the announcement banner when the close button is clicked
+    closeButton.addEventListener('click', function() {
+      announcementBanner.style.display = 'none';
+    });
+  });
 
 
 
