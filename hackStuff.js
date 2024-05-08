29af1901d8b16ab44be8e96a0da657918910b9f8
bcrhbrhcdb <apet2804@mpsedu.org>
2024-05-08 10:55:03 -0500

    a

--- hackStuff.js
+++ hackStuff.js
@@ -6,10 +6,21 @@ function executeBookmarklet(element) {
         return response.text();
       })
       .then(function(data) {
-        // Execute the bookmarklet code
-        eval(data.trim());
+        // Update the link's href with the bookmarklet code
+        element.href = data.trim();
       })
       .catch(function(error) {
         console.error('Error reading file:', error);
       });
   }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    var bookmarkletLinks = document.querySelectorAll('a[data-file-paths]');
+  
+    bookmarkletLinks.forEach(function(link) {
+      link.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent the default link behavior
+        executeBookmarklet(this);
+      });
+    });
+  });
