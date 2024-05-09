de12571da6306664df4cbb8578c2b42e1b5b8d9c
bcrhbrhcdb <apet2804@mpsedu.org>
2024-05-09 15:15:28 -0500

    a

--- announcement.js
+++ announcement.js
@@ -18,3 +18,24 @@ fetch('announcement.html') // Use a relative file path to the announcement.html 
     });
   })
   .catch(error => console.error('Error fetching announcement:', error));
+  const timeElement = document.getElementById('time');
+  const dateElement = document.getElementById('date');
+  const dayElement = document.getElementById('day'); // Add this line
+  
+  function updateTime() {
+      const now = new Date();
+      const hours = now.getHours().toString().padStart(2, '0');
+      const minutes = now.getMinutes().toString().padStart(2, '0');
+      const seconds = now.getSeconds().toString().padStart(2, '0');
+      const timeString = `${hours}:${minutes}:${seconds}`;
+      timeElement.textContent = timeString;
+  
+      const dateString = now.toDateString();
+      dateElement.textContent = dateString;
+  
+      const dayString = now.toLocaleString('default', { weekday: 'long' }); // Get day of the week
+      dayElement.textContent = dayString; // Update day element
+  }
+  
+  updateTime();
+  setInterval(updateTime, 1000);
