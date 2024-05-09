188bd3b85f418331ae3fea61b467481ff2f110f3
bcrhbrhcdb <apet2804@mpsedu.org>
2024-05-09 12:26:09 -0500

    ples

--- announcement.js
+++ announcement.js
@@ -18,20 +18,20 @@ fetch('../announcement.html') // Use a relative file path to the announcement.ht
     });
   })
   .catch(error => console.error('Error fetching announcement:', error));
-
+let container
 // Fetch the announcement content from the external HTML file
 fetch('announcement.html') // Use a relative file path to the announcement.html file
   .then(response => response.text())
   .then(data => {
     // Create a new div element to hold the announcement
-    announcementContainer = document.createElement('div');
-    announcementContainer.innerHTML = data;
+    container = document.createElement('div');
+    container.innerHTML = data;
 
     // Insert the announcement at the top of the body
-    document.body.insertBefore(announcementContainer, document.body.firstChild);
+    document.body.insertBefore(container, document.body.firstChild);
 
     // Add a click event listener to the close button
-    const closeButton = announcementContainer.querySelector('#close');
+    const closeButton = con.querySelector('#close');
     closeButton.addEventListener('click', () => {
       announcementContainer.style.display = 'none';
     });
