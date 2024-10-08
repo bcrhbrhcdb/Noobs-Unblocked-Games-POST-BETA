c0781a974a3beffd51839747a7516eda59ee0ac0
bcrhbrhcdb <bcrhbrhcdb@users.noreply.github.com>
2024-10-08 16:33:13 +0000

    deploy: 356f67dc53fc2677c4882834a28e3b1cb49ee037

---
+++ pr-preview/26/javascript/gameData.js
@@ -0,0 +1,144 @@
+const games = {
+    tetris: {
+        title: "Twitch Tetris",
+        url: "game-template.html?id=tetris",
+        originalUrl: "https://emulatoros.github.io/gfile/tetris/index.html",
+        image: "images/proxyandgameimages/gameimages/tetris.png",
+        description: "You know what this is, a ripoff of Tetris."
+    },
+    cookieClicker: {
+        title: "Cookie Clicker",
+        url: "game-template.html?id=cookieClicker",
+        originalUrl: "https://ozh.github.io/cookieclicker/",
+        image: "images/proxyandgameimages/gameimages/cookie.jpeg",
+        description: "Won't be blocked if it's on GitHub!"
+    },
+    vex9: {
+        title: "Vex 9",
+        url: "game-template.html?id=vex9",
+        originalUrl: "Ghttps://tvz2304.github.io/g16/vex-8/",
+        image: "images/proxyandgameimages/gameimages/vex9.jpeg",
+        description: "The newest game! (vex 8 as a placeholder, need a link.)"
+    },
+    vex8: {
+        title: "Vex 8",
+        url: "game-template.html?id=vex8",
+        originalUrl: "https://ubg98.github.io/Vex8/",
+        image: "images/proxyandgameimages/gameimages/vex8.jpeg",
+        description: "Eighth Game"
+    },
+    vex7: {
+        title: "Vex 7",
+        url: "game-template.html?id=vex7",
+        originalUrl: "https://ubg98.github.io/Vex7/",
+        image: "images/proxyandgameimages/gameimages/vex7.jpeg",
+        description: "The seventh game"
+    },
+    vex6: {
+        title: "Vex 6",
+        url: "game-template.html?id=vex6",
+        originalUrl: "https://ubg98.github.io/Vex6/",
+        image: "images/proxyandgameimages/gameimages/vex6.jpeg",
+        description: "The sixth game"
+    },
+    vex5: {
+        title: "Vex 5",
+        url: "game-template.html?id=vex5",
+        originalUrl: "https://vex5online.github.io/file/",
+        image: "images/proxyandgameimages/gameimages/vex5.jpeg",
+        description: "The fifth game"
+    },
+    vex4: {
+        title: "Vex 4",
+        url: "game-template.html?id=vex4",
+        originalUrl: "https://ubg98.github.io/Vex4/",
+        image: "images/proxyandgameimages/gameimages/vex4.jpeg",
+        description: "The fourth game"
+    },
+    vex3: {
+        title: "Vex 3",
+        url: "game-template.html?id=vex3",
+        originalUrl: "https://ubg98.github.io/Vex3/",
+        image: "images/proxyandgameimages/gameimages/vex3.jpeg",
+        description: "The third game"
+    },
+    bigTower1: {
+        title: "Big Tower Tiny Square 1",
+        url: "game-template.html?id=bigTower1",
+        originalUrl: "https://ubg98.github.io/BigTowerTinySquare/",
+        image: "images/proxyandgameimages/gameimages/bigsquare1.jpeg",
+        description: "Goofy Platformer."
+    },
+    bigTower2: {
+        title: "Big Tower Tiny Square 2",
+        url: "game-template.html?id=bigTower2",
+        originalUrl: "https://rebemanae.github.io/big-tower-tiny-square-2/",
+        image: "images/proxyandgameimages/gameimages/bigsquare2.jpeg",
+        description: "Even more Goofy Platformer."
+    },
+    ovo: {
+        title: "0v0",
+        url: "game-template.html?id=ovo",
+        originalUrl: "https://23azostore.github.io/s/ovo/",
+        image: "images/proxyandgameimages/gameimages/0v0.png",
+        description: "Such a Classic!"
+    },
+    evolve: {
+        title: "Evolve",
+        url: "game-template.html?id=evolve",
+        originalUrl: "https://pmotschmann.github.io/Evolve/",
+        image: "images/proxyandgameimages/gameimages/Evolve.png",
+        description: "An idle game where you Evolve your own species"
+    },
+    intoSpace2: {
+        title: "Into Space 2",
+        url: "game-template.html?id=intoSpace2",
+        originalUrl: "https://images-opensocial.googleusercontent.com/gadgets/ifr?url=https://s3.amazonaws.com/production-assetsbucket-8ljvyr1xczmb/1ee20621-61bc-4ec8-a8ec-5e839c2e6edc%2Finto-space-2.xml&container=ig",
+        image: "images/proxyandgameimages/gameimages/intospace2.png",
+        description: "Build and launch your rocket into space!"
+    },
+    paperclips: {
+        title: "Universal Paperclips",
+        url: "game-template.html?id=paperclips",
+        originalurl: "https://memalign.github.io/m/universal-paperclips/index2.html",
+        image: "images/proxyandgameimages/gameimages/paperclip.jpeg",
+        description: "Paperclips... Amazing",
+    },
+    idlegamedev: {
+        title: "Idle Idle Game Dev",
+        url: "game-template.html?id=idlegamedev",
+        originalUrl: "https://luts91.github.io/idle-idle-gamedev/index.html",
+        image: "images/proxyandgameimages/gameimages/idlegamedev.jpeg",
+        description: "A idle game revolved around making idle games. Weird."
+    },
+    idledice: {
+        title: "Idle Dice",
+        url: "game-template.html?id=idle-dice",
+        originalUrl: "https://luts91.github.io/idle-dices/index.html",
+        image: "images/proxyandgameimages/gameimages/idledice.jpeg",
+        description: "A idle game revolved around gambli- I mean rolling dice!"
+    },
+    spiceIdle: {
+        title: "Spice Idle",
+        url: "game-template.html?id=space-idle",
+        orginialUrl: "https://zakuro98.github.io/Spice-Idle/",
+        image: "images/proxyandgameimages/gameimages/spiceidle.jpeg",
+        description: "Mhm. Powder."
+    },
+    dash: {
+        title: "Geometry Dash",
+        url: "game-template.html?id=space-idle",
+        orginialUrl: "https://edug309.github.io/g8/geometry-dash/",
+        image: "images/proxyandgameimages/gameimages/geometry-dash.png",
+        description: "Scratch Rippoff"
+    },
+    bottleFlip: {
+        title: "Bottle Flip",
+        url: "game-template.html?id=bottle",
+        orginialUrl: "https://edug309.github.io/g69/bottle-flip/",
+        image: "images/proxyandgameimages/gameimages/bottle-flip.png",
+        description: "2017."
+    },
+
+
+};
