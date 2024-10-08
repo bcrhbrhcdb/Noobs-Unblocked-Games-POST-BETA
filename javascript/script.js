f29c5b789532a86a1080931b51a6fa092e30dbb6
bcrhbrhcdb <apet2804@mpsedu.org>
2024-10-08 07:54:07 -0500

    s

--- javascript/script.js
+++ javascript/script.js
@@ -3,8 +3,11 @@ function generateFooter() {
     footer.className = 'box';
     footer.innerHTML = `
         <footer>
+            <h4>Vistors!</h4>
+            <div align='center'><a href='https://www.free-website-hit-counter.com'><img src='https://www.free-website-hit-counter.com/zc.php?d=9&id=69&s=2' border='0' alt='Free Website Hit Counter'></a><br/><small><a href='https://www.free-website-hit-counter.com' title="Free Website Hit Counter"></small></div>
             <h4>Want to suggest an idea? Fill out the Google form!</h4>
             <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfjkzAiUEbMh634MSOc8yfWsP5_ZHVnF1EDE73dpTDw0Vz7SA/viewform?embedded=true" class='frame'>Loading…</iframe>   
+
         </footer>
     `;
     document.body.appendChild(footer);
