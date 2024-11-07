61f0e78c93f957517868f595ec9603bcc9ad19b8
bcrhbrhcdb <bcrhbrhcdb@users.noreply.github.com>
2024-11-07 20:09:54 +0000

    deploy: d8d659d32a262ed55d3394712b70bc6b4d4e672f

--- update-branch.sh
+++ update-branch.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Set the remote repository URL
-REMOTE_REPO="https://github.com/bcrhbrhcdb/Noobs-Unblocked-Games-POST-BETA.git"
+REMOTE_REPO="origin"
 
 # Fetch the latest changes from the remote repository
-git fetch origin
+git fetch $REMOTE_REPO
 
 # Get the current branch name
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
 # Reset the current branch to match the remote branch
-git reset --hard origin/$current_branch
+git reset --hard $REMOTE_REPO/$current_branch
