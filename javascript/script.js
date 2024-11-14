4bd29d5a335da003bd9d478aa68a77a3e71c3c05
bcrhbrhcdb <bcrhbrhcdb@users.noreply.github.com>
2024-11-14 23:03:29 +0000

    deploy: e09af99089a4b0fbe2f5a2a0231e94446a7996bb

--- javascript/script.js
+++ javascript/script.js
@@ -7,7 +7,7 @@ function generateFooter() {
             <div align='center'><a href='https://www.free-website-hit-counter.com'><img src='https://www.free-website-hit-counter.com/zc.php?d=9&id=1109&s=2' border='0' alt=''></a><br /><small><a href='https://www.free-website-hit-counter.com' title="Free Website Hit Counter"></a></small></div>
             <h4>Want to suggest an idea? Fill out the Google form!</h4>
             <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfjkzAiUEbMh634MSOc8yfWsP5_ZHVnF1EDE73dpTDw0Vz7SA/viewform?embedded=true" class='frame'>Loading…</iframe>   
-
+            <a href="https://github.com/bcrhbrhcdb/Nooby" target="_blank"><button id="navBar">The Github</button></a>
         </footer>
     `;
     document.body.appendChild(footer);
