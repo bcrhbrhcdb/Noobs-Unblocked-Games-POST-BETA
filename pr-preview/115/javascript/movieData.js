1bcef513fb2a57898597ff3463fa4bf76ff0cd53
bcrhbrhcdb <bcrhbrhcdb@users.noreply.github.com>
2024-11-27 21:35:11 +0000

    deploy: 90b936024e3688a9781162ed5231c156d63e66d8

--- pr-preview/115/javascript/movieData.js
+++ pr-preview/115/javascript/movieData.js
@@ -1,10 +1,10 @@
 const movies = {
     deadPoolAndWolve: {
         title: "Deadpool & Wolverine",
-        url: "game-template.html?id=deadPoolAndWolve",
+        url: "movie-template.html?id=deadPoolAndWolve",
         originalUrl: "https://drive.google.com/file/d/144etb6-SFvWTl89zkwfAEMNcZFYejsE_/preview",
-        image: "images/proxyandgameimages/gameimages/tetris.png",
-        description: "deadpool",
+        image: "images/movie-images/deadpool-wolverine.jpeg",
+        description: "Made in 2024, haven't watched and won't spoil",
         ratings: [],
         likes: 0,
         dislikes: 0,
