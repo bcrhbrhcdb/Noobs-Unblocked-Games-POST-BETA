336c3de977238b1df283b6a05bd86b94052badab
bcrhbrhcdb <apet2804@mpsedu.org>
2024-11-27 13:59:50 -0600

    maybe it works idk

---
+++ javascript/movieData.js
@@ -0,0 +1,12 @@
+const movies = {
+    deadPoolAndWolve: {
+        title: "Deadpool & Wolverine",
+        url: "game-template.html?id=deadPoolAndWolve",
+        originalUrl: "https://drive.google.com/file/d/144etb6-SFvWTl89zkwfAEMNcZFYejsE_/preview",
+        image: "images/proxyandgameimages/gameimages/tetris.png",
+        description: "deadpool",
+        ratings: [],
+        likes: 0,
+        dislikes: 0,
+    },
+}
