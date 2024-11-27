9ba0a5f769bb57bc45dc340ffba518a3b167115b
bcrhbrhcdb <apet2804@mpsedu.org>
2024-11-27 15:45:41 -0600

    e

--- javascript/movieData.js
+++ javascript/movieData.js
@@ -4,7 +4,7 @@ const movies = {
         url: "movie-template.html?id=deadPoolAndWolve",
         originalUrl: "https://drive.google.com/file/d/144etb6-SFvWTl89zkwfAEMNcZFYejsE_/preview",
         image: "images/movie-images/deadpool-wolverine.jpeg",
-        description: "Made in 2024, haven't watched and won't spoil",
+        description: "Made in 2024, haven't watched and won't spoil the movie!",
         ratings: [],
         likes: 0,
         dislikes: 0,
