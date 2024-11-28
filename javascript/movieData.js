d809fb98eca99c5afed2e16fab092d438bea9cf0
ekoerp1 <95361381+ekoerp1@users.noreply.github.com>
2024-11-28 13:27:42 -0700

    Update movieData.js

--- javascript/movieData.js
+++ javascript/movieData.js
@@ -22,7 +22,7 @@ const movies = {
      fnafmovie: {
         title: "The FNAF Movie",
         url: "movie-template.html?id=fnafmovie",
-        originalUrl: "https://vimeo.com/1034322497?share=copy",
+        originalUrl: "https://www.dropbox.com/scl/fi/ckir7p8ly4lvlzhzsu50i/122.mp4?rlkey=u1s04yyjymuvt46r13giso22d&st=5k46vkhc&dl=0",
         image: "images/movie-images/fnafmovie.jpeg",
         description: "Made in 2023, Pretty good movie!",
         ratings: [],
