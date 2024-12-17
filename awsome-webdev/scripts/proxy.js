88b134c27c54ce57429b622543d267c4c6b62739
awsome_webdev <rylanjarboe8@gmail.com>
2024-12-16 18:31:28 -0600

    added gauth math

--- awsome-webdev/scripts/proxy.js
+++ awsome-webdev/scripts/proxy.js
@@ -7,3 +7,10 @@ function aboutblank(event){
     frame.setAttribute('style', 'position: absolute; width: 100vw; height: 100vh; border: none; left: 0px; top: 0px;')
     win.document.body.appendChild(frame)
 }
+function aboutblank2(url){
+    const win = open()
+    const frame = document.createElement('iframe')
+    frame.src = url
+    frame.setAttribute('style', 'position: absolute; width: 100vw; height: 100vh; border: none; left: 0px; top: 0px;')
+    win.document.body.appendChild(frame)
+}
